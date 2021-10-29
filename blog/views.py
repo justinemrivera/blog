4b8793a39143c763bd11889b57f8618a5e2b6c6a
Justine Rivera <justineee.m.25@gmail.com>
2021-10-28 19:55:53 -0700

    Added mixins

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,10 @@ from django.views.generic.edit import (
     UpdateView,
     DeleteView
 )
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.urls import reverse_lazy
 from .models import Post
 
@@ -24,19 +28,27 @@ class MessageDetailView(DetailView):
     model = Post
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "body", "author"]
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ["title", "body"]
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class BlogDeleteView(DeleteView):
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "post_delete.html"
     success_url = reverse_lazy("home")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
